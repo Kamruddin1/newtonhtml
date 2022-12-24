18292c837a25cf707266e6a56822fb692df33730
kamruddin1 <ikamruddin1@gmail.com>
2022-12-24 18:02:31 +0530

    new adding file

--- its_self_practis/Dynamic css -fronted-end/main.js
+++ its_self_practis/Dynamic css -fronted-end/main.js
@@ -1,2 +1,71 @@
-let a = prompt("hey what is your name");
-console.log(a);
+let text = document.getElementById("text-container"); // text
+
+let inputField = document.getElementById("colorbox"); // input
+
+let rangeCheck = document.getElementById("rangebox"); //range
+
+console.log(rangeCheck.value);
+
+function setColor(){
+
+// console.log(inputField.value);
+
+console.log(rangeCheck.value);
+
+text.style.color = inputField.value;
+
+}
+
+function rangeFunc(){
+
+text.style.fontSize = rangeCheck.value +'px';
+
+}
+
+function textStyle(styleCheck){
+
+// console.log(styleCheck);
+
+if(styleCheck == 'underline'){
+
+if(text.style.textDecoration == styleCheck){
+
+text.style.textDecoration = null;
+
+return;
+
+}
+
+text.style.textDecoration = styleCheck;
+
+return;
+
+}
+
+if(text.style.fontStyle == styleCheck){
+
+text.style.fontStyle = null;
+
+return;
+
+}
+
+text.style.fontStyle = styleCheck;
+
+}
+
+function fn_fontFamily(){
+
+let fontFamily = document.getElementById("fontFamily");
+
+console.log(fontFamily.value);
+
+text.style.fontFamily = fontFamily.value;
+
+}
+
+function getStyle() {
+
+console.log(text.style);
+
+}
