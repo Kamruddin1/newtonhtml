bb9f7eccdacc9b7709caa55c26e3f18c6d5fc12a
kamruddin1 <ikamruddin1@gmail.com>
2022-12-24 19:52:41 +0530

    adding file

---
+++ its_self_practis/javasrcipt/if_else/hello.js
@@ -0,0 +1,49 @@
+//alert("Ansari")
+//problem-1
+
+// let age = prompt("what is your age?");
+// if(age>10 && age<20){
+//     console.log("your age lies between 10 and 20");
+
+// }
+// else{
+//     console.log("your age doesnt lies between 10 and 20");
+// }
+
+// problem -2
+// let age = prompt("what is your age");
+//     switch(age){
+//         case '10':
+//             console.log("my age is 10");
+//             break;
+//         case '11':
+//             consolelog.log("my age is 11");
+//             break;
+//         case '12':
+//             console.log("my agfe is 13");
+//             break;
+//         case '13':
+//             console.log("my age is 13");
+//             break;
+//         default:
+//             console.log("my age in invalid");
+//     }
+
+//problem number -3
+// let num = prompt("whatbis your age")
+// num = Number.parseInt(num);
+// if(num%2==0 && num%3==0){
+//     console.log("your number divisible by 2 and 3")
+// }
+// else{
+//     console.log("your number is not divible by 2 and 3")
+// }
+
+
+// problem 4
+
+// let nums = prompt("what is your number divible 2 or 3");
+// nums  = Number.parseInt(nums);
+// if(nums%2==0 || nums%3==0){
+//     console.log("your number divible by 2 and 3");
+// }
---
+++ its_self_practis/javasrcipt/loops/main.js
@@ -0,0 +1,43 @@
+// alert("ansari")
+
+// for loop
+
+// w a p add first n natural number
+// let sum=0;
+// let n = prompt("enter the value of n");
+// n = Number.parseInt(n);
+// for(let i=0; i<n; i++){
+//     sum += i+1;
+// }
+// console.log("sum of first " + n + " natural number is " + sum)
+
+// facxtorial number find
+
+// let factorial = 1;
+// let n = prompt("enter thr  value of n")
+// n = Number.parseInt(n);
+// for(let i=n; i>0; i--){
+//     factorial = factorial*i;
+// }
+// console.log("factorial number is" + factorial)
+
+
+// for in loop
+
+let obj = {
+    ansari:30,
+    srijal:31,
+    rai:32,
+    azam:31,
+    amit:33
+}
+for(let a in obj){
+     console.log("marks of" + a + " are " + obj[a]);
+}
+
+// for of loop
+for(let b of "Ansari"){
+    console.log(b)
+}
+
+
