d86aa8e60bf7f4edce3ed9b2397c39c3b13e3e6b
kamruddin1 <ikamruddin1@gmail.com>
2022-12-27 14:45:49 +0530

    adding file

---
+++ its_self_practis/javasrcipt/StringMethod/main.js
@@ -0,0 +1,9 @@
+let b = "   Ansari"
+let a  =  "Ansari"
+// length
+console.log(b.length)
+// find the postion of index
+console.log(b[2])
+console.log(b.trim())
+console.log(b.concat (a))
+console.log
