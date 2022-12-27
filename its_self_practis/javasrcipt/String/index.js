f77b2aaea27a0474e18c5212332de114d58b562c
kamruddin1 <ikamruddin1@gmail.com>
2022-12-27 14:10:46 +0530

    adding file

---
+++ its_self_practis/javasrcipt/String/index.js
@@ -0,0 +1,21 @@
+// let a  = "Ansari"
+// // console.log(a.length)
+// alert(a.length)
+// alert(a[0])
+// temlate litral
+let  myfrnd = `srijal`
+let myfrnd2 = `anamika`
+let frnd = `${myfrnd}  ${myfrnd2}`
+console.log(frnd)
+// escape sqeuance
+let a1 = 'ansar\'i'
+let a2 = "ansar\"i"
+console.log(a1)
+console.log(a2)
+let a3 = "ansar\ni"
+let a4 = "ansar\ri"
+let a5 = "ansar\ti"
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a5.length)
---
+++ its_self_practis/javasrcipt/functional&loopsProblem/index.js
@@ -0,0 +1,8 @@
+let marks = {
+    Ansari: 80,
+    srijal: 81,
+    rai:  80
+}
+for(let key=0; key<Object.keys.length ; key++){
+    console.log(Object.keys(marks[i] + marks(Object.keys(i))))
+}
