c53ae6ccc9f0f818fa9fd9b0cba088aee99e21f7
kamruddin1 <ikamruddin1@gmail.com>
2022-12-29 14:42:31 +0530

    adding file

---
+++ its_self_practis/javasrcipt/arrays/main.js
@@ -0,0 +1,55 @@
+// let marks_class_12 = [90, 92, 68, 80 ,true , false ,"ansari"]
+// console.log(marks_class_12)
+// console.log(marks_class_12.length)
+// console.log(marks_class_12[0])
+// console.log(marks_class_12[1])
+// console.log(marks_class_12[2])
+// console.log(marks_class_12[3])
+// console.log(marks_class_12[4])
+// console.log(marks_class_12[5])
+// console.log(marks_class_12[6])
+// console.log(marks_class_12[7])
+
+
+// // addding a new marks in arrays
+// marks_class_12[6] = 81
+// console.log(marks_class_12)
+// // replace the marks of an arrays first index
+// marks_class_12[0] = 91
+// console.log(marks_class_12)
+// console.log(typeof marks_class_12)
+
+
+
+/* arrays method
+1- integer to string convert
+2- join
+3- push
+4- pop
+5- shift
+6- unshift
+7- how to check type of objects
+[typeof..... var name put here]
+
+
+*/
+
+// let num = [10, 20, 30, 40, 50, 60]
+// // 1
+// let a = num.toString()
+// console.log(a, typeof a)
+// // 2 
+// let b = num.join(" and ")
+// console.log(b)
+// // 3
+// let c = num.push=[70]
+// console.log(c,num)
+// //4
+// let d = num.pop()
+// console.log(d,num)
+// //5
+// let e = num.shift()
+// console.log(e,num)
+// // 6
+// let f = num.unshift()
+// console.log(num,f)
