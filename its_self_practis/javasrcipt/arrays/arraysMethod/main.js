72d4121e96d4c151fcef0a35892e898bbb417121
kamruddin1 <ikamruddin1@gmail.com>
2022-12-30 12:50:46 +0530

    adding file

---
+++ its_self_practis/javasrcipt/arrays/arraysMethod/main.js
@@ -0,0 +1,20 @@
+//  let num = [10, 20, 30, 40, 50]
+// let num1= [60,70, 80,90]
+// let num3=num.concat(num1) 
+// console.log(num3)
+
+// reverse
+// num.reverse()
+// console.log(num)
+
+// splice
+// means that kaha se start krna hain aur kaha se delete krna hain aur kitna new elements add krna hain
+
+// num.splice(2,3,100, 200, 300)
+// console.log(num)
+
+// slice
+
+
+//console.log(num.slice(2))
+    
