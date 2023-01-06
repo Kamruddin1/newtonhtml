1818f0762b9a9f5aca74d3536351717ce6dbc13c
kamruddin1 <ikamruddin1@gmail.com>
2023-01-06 20:12:58 +0530

    addind file

---
+++ its_self_practis/form-js/script.js
@@ -0,0 +1,7 @@
+let n = document.getElementsById("name");
+let no = document.getElementsById("number");
+let mo = document.getElementsById("mobile");
+
+function onSubmit(){
+    console.log(name.value, number.value, mobile.value);
+}
