7bc8082da69bf3b53d4e87fc9b67d41b6563b72f
kamruddin1 <ikamruddin1@gmail.com>
2023-01-10 20:01:53 +0530

    adding file

---
+++ its_self_practis/javasrcipt/loop_in _arrays/script.js
@@ -0,0 +1,36 @@
+// alert("Ansari")
+
+// // arrays print 
+// let num = [10, 20, 30]
+// for(let i=0; i<num.length; i++){
+//    console.log(num[i])
+// }
+
+// for each loop in  arrays 
+
+// num.forEach((element)=>{
+//     console.log(element*element)
+// })
+ 
+
+//Array.from
+
+// let name = "Ansari"
+// let arr = Array.from(name)
+//     console.log(arr)
+
+// for of in arrays
+
+// for(let i of num){
+//     console.log(i)
+// }
+
+// // for in loop in arrays
+
+// for(let j in num){ // keys
+//     console.log(j)
+// }
+
+// for(let j in num){
+//     console.log(num[j])
+// }
