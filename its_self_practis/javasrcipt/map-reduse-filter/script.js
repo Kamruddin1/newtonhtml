f4abbbc3652dc365adfc00d259b0aed704b89637
kamruddin1 <ikamruddin1@gmail.com>
2023-01-10 21:08:38 +0530

    adding file

---
+++ its_self_practis/javasrcipt/map-reduse-filter/script.js
@@ -0,0 +1,29 @@
+//alert("ansari")
+let arr = [10, 20, 30, 40]
+// console.log(arr)
+
+// map fun
+
+// map function create a new arrays
+
+// let a = arr.map((value, index  )=>{
+//     console.log(value,index)
+//     return value
+// })
+// console.log(a)
+
+// filter
+// filter the some elemnets like greater then less then
+
+// let arr2 = [10, 20, 30, 40, 50]
+// let a1=arr.filter((a)=>{
+   
+//     return a <30
+// })
+// console.log(a1)
+
+let arr3 = [1 , 2, 3, 4, 5]
+let a1 = arr3.reduce((h1, h2)=>{
+    return h1 + h2
+})
+console.log(a1)
