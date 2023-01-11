6724190496a15f026788cea0572fedaa382e6c52
kamruddin1 <ikamruddin1@gmail.com>
2023-01-11 15:13:29 +0530

    adding file

---
+++ its_self_practis/javasrcipt/alret_confirm_prompt/script.js
@@ -0,0 +1,26 @@
+// alert("Ansari")
+
+
+
+// prompt
+// alert("enter the value of a")
+// let a =  prompt("enter the any value")
+// a= Number.parseInt(a)
+// alert("entered type of a " +(typeof a))
+// document.write(a)
+
+
+// confirm
+
+alert("enter the value of a")
+let a =  prompt("enter the any value")
+a= Number.parseInt(a)
+alert("entered type of a " +(typeof a))
+document.write(a)
+let write = confirm("do you want to load this page")
+if(write){
+    document.write(a)
+}
+else{
+    document.write("  please allow me ")
+}
