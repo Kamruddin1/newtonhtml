fbe11155d55660634e338e0b83fdbe3f5e193132
kamruddin1 <ikamruddin1@gmail.com>
2023-01-23 12:43:29 +0530

    adding file

---
+++ its_self_practis/javasrcipt/js-practisSheet/navbar/main.js
@@ -0,0 +1,2 @@
+let nav = document.getElementsByClassName("container")[1].firstElementChild.firstElementChild.style.color="red";
+let nav1 =  document.getElementsByClassName.getElementsByClassName("container")[1].firstElementChild.style.color="blue";
