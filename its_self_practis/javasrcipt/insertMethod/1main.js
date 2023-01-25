4c4e811a8b4501975d027a5362a2d5f6bbba39f2
kamruddin1 <ikamruddin1@gmail.com>
2023-01-25 14:31:20 +0530

    adding file

---
+++ its_self_practis/javasrcipt/insertMethod/1main.js
@@ -0,0 +1,16 @@
+// let a = document.getElementsByTagName('div')[0]
+// a.innerHTML = a.innerHTML + '<h1>hello world !</h1>'
+// dono same work karta h
+
+
+// appendchild ka use loop me kiya jata h
+
+
+let b = document.createElement('div')
+b.innerHTML ='<h2>hello world !</h2>';
+// b.appendChild('div');
+
+// b.prepend('div')// prerpend means container ke under hi rhega
+// b.before('div')
+b.after('div')
+
